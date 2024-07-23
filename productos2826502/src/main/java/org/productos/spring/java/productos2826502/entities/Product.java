1e2c19dcbcadc4a85089f1c9a32612cee9fa005f
Aprendiz <Aprendiz@BOGDFPCGMF4146.FormacionSena.RED>
2024-07-23 15:39:34 -0500

    configuracion de base de datos

--- productos2826502/src/main/java/org/productos/spring/java/productos2826502/entities/Product.java
+++ productos2826502/src/main/java/org/productos/spring/java/productos2826502/entities/Product.java
@@ -1,11 +1,17 @@
 package org.productos.spring.java.productos2826502.entities;
 
-import lombok.*;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
 @Setter
 @Builder
+@Entity
+@Table(name="products")
 public class Product {
 
     private Long id;
